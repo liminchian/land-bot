a28fb5d9e60e6afb3566a9a9cdbb2082aca6351a
liminchian <liminchian@gmail.com>
2024-07-16 23:19:07 +0800

    整理資料庫欄位

--- crates/migration/src/m20220101_000001_create_table.rs
+++ crates/migration/src/m20220101_000001_create_table.rs
@@ -9,11 +9,38 @@ impl MigrationTrait for Migration {
         manager
             .create_table(
                 Table::create()
-                    .table(Moi::Table)
+                    .table(QuestionAnswer::Table)
                     .if_not_exists()
-                    .col(ColumnDef::new(Moi::Id).uuid().not_null().primary_key())
-                    .col(ColumnDef::new(Moi::Question).string().not_null())
-                    .col(ColumnDef::new(Moi::Answer).string().not_null())
+                    .col(
+                        ColumnDef::new(QuestionAnswer::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(QuestionAnswer::Source).string().not_null())
+                    .col(ColumnDef::new(QuestionAnswer::Question).string())
+                    .col(ColumnDef::new(QuestionAnswer::Answer).string())
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_table(
+                Table::create()
+                    .table(Conversation::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Conversation::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Conversation::Role).string().not_null())
+                    .col(
+                        ColumnDef::new(Conversation::Datetime)
+                            .date_time()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Conversation::Content).string())
                     .to_owned(),
             )
             .await
@@ -21,15 +48,28 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Moi::Table).to_owned())
+            .drop_table(Table::drop().table(QuestionAnswer::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(Conversation::Table).to_owned())
             .await
     }
 }
 
-#[derive(DeriveIden)]
-enum Moi {
+#[derive(Iden)]
+enum QuestionAnswer {
     Table,
     Id,
+    Source,
     Question,
     Answer,
+}
+
+#[derive(Iden)]
+enum Conversation {
+    Table,
+    Id,
+    Role,
+    Content,
+    Datetime,
 }
