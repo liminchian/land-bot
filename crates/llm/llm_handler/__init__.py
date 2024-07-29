b2461de79b1edcf514efa67b7b095ad7d998e640
liminchian <liminchian@gmail.com>
2024-07-29 22:37:50 +0800

    完成基本的推論流程

--- crates/llm/llm_handler/__init__.py
+++ crates/llm/llm_handler/__init__.py
@@ -1,33 +1,39 @@
-from langchain_community.llms.llamacpp import LlamaCpp
-from langchain_core.language_models.llms import LLM
-from langchain_core.runnables import RunnablePassthrough
-from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers.string import StrOutputParser
+import warnings
+from typing import Generator, Iterator
 
-def create_qa_chain(llm: LLM):
-    prompt = PromptTemplate.from_template("""
-請依據問題來回答，並以三點以內的觀點來結構化輸出的內容，不需要額外說明：
-{question}
-""")
-    chain = {"question": RunnablePassthrough()} | prompt | llm | StrOutputParser()
-    return chain
+from llama_cpp import Llama
 
-def create_json_response_chain(llm: LLM):
-    ...
 
-def create_openai_chat_chain(llm: LLM):
-    ...
+def inference(llm: Llama, prompt: str, **kwargs) -> str:
+    if "stream" in kwargs and kwargs["stream"]:
+        warnings.warn(Warning("發現 stream 在參數中，預設為單一回應而非串流回應。"))
+        del kwargs["stream"]
 
-def create_rag_chain(llm: LLM):
-    ...
+    response = llm(prompt, **kwargs)
 
-def load_gguf_model(model_path: str) -> LlamaCpp:
-    return LlamaCpp(
-        model_path=model_path,
-        n_ctx=512,
-        max_tokens=2048,
-        n_gpu_layers=12,
-        verbose=False,
-        stop=["<|eot_id|>"]
-    ) # type: ignore
+    try:
+        assert isinstance(response, dict), "不是正常的回應"
+        return "\n".join(ch["text"] for ch in response["choices"])
+    except AssertionError as e:
+        raise e
 
+
+def streaming(llm: Llama, prompt: str, **kwargs) -> Generator[str, None, None]:
+    if "stream" in kwargs and kwargs["stream"]:
+        warnings.warn(Warning("發現 stream 在參數中，預設為串流回應而非單一回應。"))
+        del kwargs["stream"]
+
+    streaming = llm(prompt, **kwargs)
+
+    try:
+        assert isinstance(streaming, Iterator), "不是 streaming"
+        for res in streaming:
+            assert isinstance(res, dict), "不是正常的回應"
+            result = "\n".join(ch["text"] for ch in res["choices"])
+            yield result
+    except AssertionError as e:
+        raise e
+
+
+def quantize_model(model_path: str, **kwargs) -> Llama:
+    return Llama(model_path=model_path, **kwargs)  # type: ignore
--- crates/llm/src/main.rs
+++ crates/llm/src/main.rs
@@ -1,23 +1,40 @@
 use pyo3::prelude::*;
+use pyo3::types::{IntoPyDict, PyList};
 
 fn main() -> PyResult<()> {
     Python::with_gil(|py| {
-        let llm_handler = py.import_bound("llm_handler").unwrap();
+        let llm_handler = py.import_bound("llm_handler").expect("導入套件時發生錯誤");
+        let prompt = format!(
+            r#"
+請簡潔回答以下問題，無需額外解釋：
+問題：{}
+        "#,
+            "台灣的國慶日是幾月幾號？"
+        );
+
+        let model_kwargs = [
+            (
+                "model_path",
+                "/home/sei/Program/llm-models/taide-8b-a.3-q4_k_m.gguf".into_py(py),
+            ),
+            ("n_gpu_layers", 12.into_py(py)),
+            ("verbose", false.into_py(py)),
+        ]
+        .into_py_dict_bound(py);
 
         let llm = llm_handler
-            .call_method1(
-                "load_gguf_model",
-                ("/home/sei/Program/llm-models/taide-8b-a.3-q4_k_m.gguf",),
-            )
+            .call_method("quantize_model", (), Some(&model_kwargs))
             .expect("讀取 llm 的過程發生錯誤");
 
-        let chain = llm_handler
-            .call_method1("create_qa_chain", (llm,))
-            .expect("建立 chain 的過程發生錯誤");
+        let call_kwargs = [(
+            "stop",
+            PyList::new_bound(py, vec!["\n", "問題:", "<|eot_id|>"]),
+        )]
+        .into_py_dict_bound(py);
 
-        let result = chain
-            .call_method1("invoke", ("請問台灣最嚴重的颱風？",))
-            .expect("推論的過程發生錯誤");
+        let result = llm_handler
+            .call_method("inference", (llm, prompt), Some(&call_kwargs))
+            .expect("推論過程發生錯誤");
 
         println!("{}", result.to_string());
     });
